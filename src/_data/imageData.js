365042e9ab2c74db9139b438f9950a84924c5b8f
Scott Smith <stsmith@callutheran.edu>
2021-05-11 10:10:31 -0700

    correctly cache cloudinary API calls

--- src/_data/imageData.js
+++ src/_data/imageData.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
+
+const { AssetCache } = require("@11ty/eleventy-cache-assets");
 const cloudinary = require('cloudinary');
-const fs = require('fs');
 
-
+const cacheID = 'cloud_image_meta';
 
 module.exports = async function(){
 
@@ -11,36 +12,51 @@ module.exports = async function(){
 		  api_key: process.env.CLOUD_KEY, 
 		  api_secret: process.env.CLOUD_SECRET
 	});
+
+	let asset = new AssetCache(cacheID);
+
+	if(asset.isCacheValid("7d")) {
+		console.log('cache accesssed');
+		return asset.getCachedValue();
+	} else {
 	
-	const cloudImageData = cloudinary.v2.search.expression('trailcoffee').max_results(500).execute().then(result => {
-		
-		console.log('cloudinary accessed');
-		
-		let jsonData = [];
-	
-		for(const i in result.resources){
-			const resultItem = result.resources[i];
+		function getData() {
+			return new Promise(function (resolve, reject) {
+				cloudinary.v2.search.expression('trailcoffee').max_results(500).execute().then(result => {
+				
+					console.log('calling cloudinary API');
+					
+					let jsonData = {};
+				
+					for(const i in result.resources){
+						const resultItem = result.resources[i];
 
-			const key = resultItem.public_id;
+						const key = resultItem.public_id;
 
-			jsonData[key] = {
-				'public_id': resultItem.public_id,
-				'width': resultItem.width,
-				'height': resultItem.height,
-				'format': resultItem.format,
-				'version': resultItem.version
-			};
+						jsonData[key] = {
+							'public_id': resultItem.public_id,
+							'width': resultItem.width,
+							'height': resultItem.height,
+							'format': resultItem.format,
+							'version': resultItem.version
+						};
 
+					};
+
+					resolve(jsonData);
+				});
+			});
 		};
 
-		
-		return jsonData;
-		
-		
-	});
 
-	fs.writeFileSync('./image-data.json', cloudImageData);
+		return getData().then(data => {
+			console.log('cloudinary accessed');
+			asset.save([JSON.stringify(data)], cacheID); 
+			return data
+		});
 
-	return cloudImageData;
+	}
+
+
 
 }
